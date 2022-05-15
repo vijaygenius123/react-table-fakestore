203e1dc3388fafceb71ead6c983af4dc4c4c8cec
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-05-15 13:51:20 +0100

    Add tablehook for edit and sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import {useTable} from 'react-table'
+import {useTable, useSortBy} from 'react-table'
+import cn from 'classnames'
 import axios from 'axios';
 
 function App() {
@@ -14,44 +15,74 @@ function App() {
         fetchProducts()
     }, [])
 
-    const columns = useMemo(() => ([
-        {
-            Header: 'Id',
-            accessor: 'id'
-        },
-        {
-            Header: 'Price',
-            accessor: 'price'
-        },
-        {
-            Header: 'Title',
-            accessor: "title"
-        }
-    ]), [])
+    const productsData = useMemo(() => [...products], [products])
+    const productsColumn = useMemo(() => products[0] ? Object.keys(products[0])
+        .filter((key) => key !== "rating")
+        .map(key => {
+            if (key === 'image') {
+                return {
+                    Header: key,
+                    accessor: key,
+                    Cell: ({value}: { value: string }) => {
+                        return <img src={value} alt={value}/>;
+                    },
+                    maxSize: 40
+                }
+            }
+            return {
+                Header: key,
+                accessor: key
+            }
+        }) : [], [products])
+
+    const tableHooks = (hooks: { visibleColumns: ((columns: any) => void)[]; }) => {
+        hooks.visibleColumns.push((columns) => [
+            ...columns, {
+                id: 'edit',
+                Header: 'Edit',
+                Cell: ({row}: { row: { id: string } }) => (
+                    <button onClick={() => {
+                        alert(`Clicked on ${row?.id}`)
+                    }} className={"px-2 py-1 inline-block outline-0"}> Edit </button>
+                )
+            }
+        ])
+    }
 
     // @ts-ignore
-    const tableInstance = useTable({columns, data: products})
+    const tableInstance = useTable({columns: productsColumn, data: productsData}, tableHooks, useSortBy)
+
     const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = tableInstance;
+    // @ts-ignore
     return (
-        <div className="container">
+        <div className="container mx-auto py-4">
+            <h1 className={"text-2xl text-center"}>Products</h1>
             <table {...getTableProps} className={"table-auto min-w-full text-center border"}>
                 <thead>
                 {headerGroups.map((headerGroup,) => (
                     <tr {...headerGroup.getHeaderGroupProps()} className={"border-b"}>
                         {
                             headerGroup.headers?.map(column => (
-                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                                //@ts-ignore
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}
+                                    className={"border-b"}>
+                                    {column.render("Header")}
+                                    {
+                                        //@ts-ignore
+                                        column.isSorted ? (column.isSortedDesc ? "↓" : "↑") : "" }
+                                </th>
                             ))
                         }
                     </tr>
                 ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
+                {rows.map((row, idx) => {
                     prepareRow(row)
-                    return <tr {...row.getRowProps()}>{
+                    return <tr {...row.getRowProps()}
+                               className={cn({' bg-green-200': idx % 2 === 0}, 'border', 'bg-opacity-30')}>{
                         row.cells.map((cell,) =>
-                            <td {...cell.getCellProps()}>{cell.render("Cell")}</td>)
+                            <td {...cell.getCellProps()} className={"border"}>{cell.render("Cell")}</td>)
                     }</tr>
                 })}
                 </tbody>
